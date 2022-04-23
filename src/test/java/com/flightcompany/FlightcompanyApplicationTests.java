d8803da14b9b4ec4cca5734d626df92fa18854dc
Pedro <drivaspedro@gmail.com>
2022-04-22 22:18:03 -0300

    initial commit

---
+++ src/test/java/com/flightcompany/FlightcompanyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.flightcompany;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FlightcompanyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
